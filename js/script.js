ce7475c2cf1769d1d836f4ce53f2c4e196aa0dbf
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-21 16:39:44 +0200

    Add reset button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // console.log('js ok');
 
-//Recupero Submit
+//Recupero Submit e Annulla
 const submitBtn = document.querySelector('input[type="submit"]');
 console.log('Variabile con il submit: ', submitBtn);
+
+const resetBtn = document.querySelector('button[type="reset"]');
+console.log('Variabile con il reset: ', resetBtn);
 
 let price;
 
@@ -26,25 +29,39 @@ submitBtn.addEventListener('click', function () {
     // SE è presente il nome
     if (userName != '') {
         //- SE i km sono presenti e sono un numero
-        if (userKm != '' && !isNaN(userKm)) {
+        if (userKm != '' && !isNaN(userKm) && userKm > 0) {
             //- Calcola il prezzo del biglietto
             price = 0.21 * userKm;
             //- SE l'utente è minorenne (userAge < 18anni) applica uno sconto del 20%
             //- SE l'utente ha più di 65 anni (userAge > 65anni) applica uno sconto del 40%
-            price -= price * userDiscount
+            price -= price * userDiscount;
+            //- Arrotonda prezzo a 2 decimal
+            price = parseFloat(price.toFixed(2));
             console.log('Prezzo del biglietto: ', price);
-
+            //- ALTRIMENTI dai errore
         } else {
-            alert("ERRORE KM");
+            alert("Inserisci correttamente il numero di kilometri che vuoi percorrere");
             console.log(userKm);
 
         }
 
     } else {
-        alert("ERRORE NOME");
+        alert("Inserisci nome e cognome");
         console.log(userName);
     }
 })
 
-//                 - Arrotonda prezzo a 2 decimal
-//     - ALTRIMENTI dai errore
+resetBtn.addEventListener('click', function () {
+
+    let userName = document.querySelector('input[name="userName"]');
+    userName.value = '';
+    console.log('Variabile con lo username: ', userName);
+
+    let userKm = document.querySelector('input[name="userKm"]');
+    userKm.value = '';
+    console.log('Variabile con i km necessari: ', userKm);
+
+    let userDiscount = document.querySelector('select[name="userDiscount"]');
+    userDiscount.value = 0;
+    console.log('Variabile con lo sconto: ', userDiscount);
+})
